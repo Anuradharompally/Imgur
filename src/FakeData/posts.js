4a20c1e6cf1d755229f27ce599312fe35b4fead8
anuradharompally <anuradha.07anu@gmail.com>
2021-03-11 20:19:20 +0530

    adding Imgur Files

---
+++ src/FakeData/posts.js
@@ -0,0 +1,109 @@
+export default [
+	{
+		id:0,
+		title:"image from imgur",
+		img:"https:\\\\i.imgur.com/t/memes/i2g1itt.jpeg",
+		author:'Anuradha Divya',
+		upVotes:140,
+		comments:[
+			{
+                id:0,
+				comment:'this is the best post i have seen!!',
+				author:'Anuradha'
+			},
+            {
+                id:1,
+                comment:'this is the best post i have seen!! on the best page in world',
+                author:'Divya'
+            }
+		],
+		views:0
+	},
+    {
+    	id:1,
+    	title:"Seeking active players!",
+    	img:"https:\\\\i.imgur.com/2ewtXxQ.jpg",
+    	author:'Anuradha',
+    	upVotes:24,
+    	comments:[
+    		{
+                id:0,
+				comment:'this is the best post i have seen!!',
+				author:'Divya'
+			}
+    	],
+		views:0
+    },
+    {
+    	id:2,
+    	title:"Seeking active players!",
+    	img:"https:\\\\i.imgur.com/tAQ5j6G.jpeg",
+    	author:'Divya',
+    	upVotes:444,
+    	comments:[],
+		views:0
+    },
+    {
+    	id:3,
+    	title:"image from imgur",
+    	img:"https:\\\\i.imgur.com/HSo8e0g.jpeg",
+    	author:'Anuradha',
+    	upVotes:4,
+    	comments:[
+    		{
+                id:0,
+				comment:'this is the best post i have seen!!',
+				author:'Divya'
+			}
+    	],
+		views:0
+    },
+    {
+    	id:4,
+    	title:"Seeking active players!",
+    	img:"https:\\\\i.imgur.com/d8HfE1K.jpg",
+    	author:'Anuradha1',
+    	upVotes:124,
+    	comments:[
+    		{
+                id:0,
+				comment:'this is the best post i have seen!!',
+				author:'Divya'
+			}
+    	],
+		views:0
+    },
+    {
+    	id:5,
+    	title:"Seeking active players!",
+    	img:"https:\\\\i.imgur.com/aB3NUoc.jpeg",
+    	author:'Divya1',
+    	upVotes:124,
+    	comments:[],
+		views:0
+    },
+    {
+    	id:6,
+    	title:"image from imgur",
+    	img:"https:\\\\i.imgur.com/eB0wQz8.jpg",
+    	author:'Anuradha',
+    	upVotes:43,
+    	comments:[],
+		views:0
+    },
+    {
+    	id:7,
+    	title:"its time to speak more!",
+    	img:"https:\\\\i.imgur.com/Itx3dR0.jpg",
+    	author:'Divya',
+    	upVotes:94,
+    	comments:[
+    		{
+                id:0,
+				comment:'this is the best post i have seen!!',
+				author:'Divya2'
+			}
+    	],
+		views:0
+    }
+];
